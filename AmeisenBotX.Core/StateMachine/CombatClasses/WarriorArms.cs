4fb3e83e2fbd1a98f680a6b066f1de5c79cc6728
Jamsbaer <0tuner0@gmail.com>
2019-08-22 21:14:00 +0200

    Basic Warrior Arm Class

--- AmeisenBotX.Core/StateMachine/CombatClasses/WarriorArms.cs
+++ AmeisenBotX.Core/StateMachine/CombatClasses/WarriorArms.cs
@@ -20,6 +20,8 @@ namespace AmeisenBotX.Core.StateMachine.CombatClasses
 
         private WowPosition LastPosition { get; set; }
 
+        private DateTime HeroicStrikeLastUsed { get; set; }
+
         public WarriorArms(ObjectManager objectManager, CharacterManager characterManager, HookManager hookManager)
         {
             ObjectManager = objectManager;
@@ -46,19 +48,84 @@ namespace AmeisenBotX.Core.StateMachine.CombatClasses
                 HookManager.StartAutoAttack();
             }
 
+            double rage = ObjectManager.Player.Rage;
             double distance = ObjectManager.Player.Position.GetDistance(target.Position);
+            double healthpercent = ((double)target.Health / (double)target.MaxHealth) * 100;
+            double healthpercentme = ((double)ObjectManager.Player.Health / (double)ObjectManager.Player.MaxHealth) * 100.0;
 
-            if (distance > 8 && distance < 25)
+            if (HookManager.GetSpellCooldown("Enraged Regeneration") <= 0 
+                && rage >= 15 && healthpercentme <= 40)
+            {
+                HookManager.CastSpell("Enraged Regeneration");
+            }
+
+            if (HookManager.GetSpellCooldown("Intimidating Shout") <= 0 
+                && rage >= 25 && healthpercentme <= 40)
+            {
+                HookManager.CastSpell("Intimidating Shout");
+            }
+
+            if (HookManager.GetSpellCooldown("charge") <= 0 
+                && distance > 8 && distance < 25)
             {
                 HookManager.CastSpell("charge");
             }
+
+            if (HookManager.GetSpellCooldown("Bladestorm") <= 0
+            && rage >= 25)
+            {
+                HookManager.CastSpell("Bladestorm");
+            }
+
+            if (HookManager.GetSpellCooldown("Mortal Strike") <= 0 
+                && rage >= 30 && distance < 3)
+            {
+                HookManager.CastSpell("Mortal Strike");
+            }
+
+            if (HookManager.GetSpellCooldown("Pummel") <= 0
+            && rage >= 10 && distance < 3)
+            {
+                HookManager.CastSpell("Pummel");
+            }
+
+            if (HeroicStrikeLastUsed + TimeSpan.FromSeconds(6) < DateTime.Now
+                && rage >= 12 && distance < 3)
+            {
+                HookManager.CastSpell("Heroic Strike");
+                HeroicStrikeLastUsed = DateTime.Now;
+            }
+
+            if (target.IsFleeing && distance < 3 && rage >= 12)
+            {
+                HookManager.CastSpell("Hamstring");
+            }
+
+            if (rage >= 15 && distance < 3 && healthpercent <= 20)
+            {
+                HookManager.CastSpell("Execute");
+            }
+
+            if (HookManager.GetSpellCooldown("Berserker Rage") <= 0
+            && ObjectManager.Player.IsConfused || ObjectManager.Player.IsDazed || ObjectManager.Player.IsSilenced
+            || ObjectManager.Player.IsFleeing)
+            {
+                HookManager.CastSpell("Berserker Rage");
+            }
+
+            if (HookManager.GetSpellCooldown("Heroic Throw") <= 0
+            && distance <= 30 && distance >= 10)
+            {
+                HookManager.CastSpell("Heroic Throw");
+            }
+
         }
 
         private void HandleMovement(WowUnit target)
         {
             double distanceTravel = ObjectManager.Player.Position.GetDistance(LastPosition);
             CharacterManager.MoveToPosition(target.Position);
-            if(distanceTravel < 0.01)
+            if(distanceTravel < 0.001 && distanceTravel > 0)
             {
                 CharacterManager.Jump();
             }
