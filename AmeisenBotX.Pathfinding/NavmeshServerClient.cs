1fee69d43bffe8d76f3cf2a75f86d002787a9e9c
Jannis Höschele <jannis@jnns.de>
2019-10-15 18:08:29 +0200

    auto reconnect fix at the navmesh client

--- AmeisenBotX.Pathfinding/NavmeshServerClient.cs
+++ AmeisenBotX.Pathfinding/NavmeshServerClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -11,104 +12,118 @@ namespace AmeisenBotX.Pathfinding
 {
     public class NavmeshServerClient : IPathfindingHandler
     {
-        public NavmeshServerClient(string ip, int port)
-        {
-            Ip = IPAddress.Parse(ip);
-            Port = port;
-            TcpClient = new TcpClient();
-
-            ConnectionWatchdog = new Timer(1000);
-            ConnectionWatchdog.Elapsed += CConnectionWatchdog;
-            ConnectionWatchdog.Start();
-        }
-
-        public IPAddress Ip { get; }
-
-        public bool IsConnected { get; private set; }
-
-        public int Port { get; }
-
-        public TcpClient TcpClient { get; }
-
-        private Timer ConnectionWatchdog { get; }
-
-        public void Disconnect()
-        {
-            ConnectionWatchdog.Stop();
-            TcpClient.Close();
-        }
-
-        public List<Vector3> GetPath(int mapId, Vector3 start, Vector3 end)
-        {
-            if (!TcpClient.Connected)
+            public NavmeshServerClient(string ip, int port)
             {
+                Ip = IPAddress.Parse(ip);
+                Port = port;
+                TcpClient = new TcpClient();
+
+                ConnectionWatchdog = new Timer(1000);
+                ConnectionWatchdog.Elapsed += CConnectionWatchdog;
+                ConnectionWatchdog.Start();
+            }
+
+            public IPAddress Ip { get; }
+
+            public bool IsConnected { get; private set; }
+
+            public int Port { get; }
+
+            public TcpClient TcpClient { get; private set; }
+
+            private Timer ConnectionWatchdog { get; }
+
+            private StreamReader Reader { get; set; }
+
+            private StreamWriter Writer { get; set; }
+
+            public void Disconnect()
+            {
+                ConnectionWatchdog.Stop();
+                Reader.Close();
+                Writer.Close();
+                TcpClient.Close();
+            }
+
+            public List<Vector3> GetPath(int mapId, Vector3 start, Vector3 end)
+            {
+                if (TcpClient.Connected)
+                {
+                    try
+                    {
+                        string pathRequest = JsonConvert.SerializeObject(new PathRequest(start, end, mapId));
+
+                        Writer.WriteLine(pathRequest + " &gt;");
+                        Writer.Flush();
+
+                        string pathJson = Reader.ReadLine().Replace("&gt;", string.Empty);
+
+                        if (IsValidJson(pathJson))
+                        {
+                            return JsonConvert.DeserializeObject<List<Vector3>>(pathJson.Trim());
+                        }
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                }
+
                 return new List<Vector3>();
             }
 
-            using (StreamReader reader = new StreamReader(TcpClient.GetStream(), Encoding.ASCII))
-            using (StreamWriter writer = new StreamWriter(TcpClient.GetStream(), Encoding.ASCII))
+            public bool IsInLineOfSight(Vector3 start, Vector3 end, int mapId)
             {
-                string pathRequest = JsonConvert.SerializeObject(new PathRequest(start, end, mapId));
-
-                writer.WriteLine(pathRequest + " &gt;");
-                writer.Flush();
-
-                string pathJson = reader.ReadLine().Replace("&gt;", string.Empty);
-
-                if (IsValidJson(pathJson))
-                {
-                    return JsonConvert.DeserializeObject<List<Vector3>>(pathJson.Trim());
-                }
-                else
-                {
-                    return new List<Vector3>();
-                }
-            }
-        }
-
-        public bool IsInLineOfSight(Vector3 start, Vector3 end, int mapId)
-        {
-            return GetPath(mapId, start, end).Count == 1;
-        }
-
-        private static bool IsValidJson(string strInput)
-        {
-            strInput = strInput.Trim();
-            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) 
-                || (strInput.StartsWith("[") && strInput.EndsWith("]")))
-            {
-                try
-                {
-                    JToken obj = JToken.Parse(strInput);
-                    return true;
-                }
-                catch
-                {
-                    // ignored
-                }
+                return GetPath(mapId, start, end).Count == 1;
             }
 
-            return false;
-        }
-
-        private void CConnectionWatchdog(object sender, ElapsedEventArgs e)
-        {
-            if (!TcpClient.Connected)
+            private static bool IsValidJson(string strInput)
             {
-                try
+                strInput = strInput.Trim();
+                if ((strInput.StartsWith("{") && strInput.EndsWith("}"))
+                    || (strInput.StartsWith("[") && strInput.EndsWith("]")))
                 {
-                    TcpClient.Connect(Ip, Port);
+                    try
+                    {
+                        JToken obj = JToken.Parse(strInput);
+                        return true;
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
                 }
-                catch
-                {
-                    // server is maybe not running or whatever
-                }
+
+                return false;
             }
 
-            if (TcpClient?.Client != null)
+            private void CConnectionWatchdog(object sender, ElapsedEventArgs e)
             {
-                IsConnected = TcpClient.Connected;
+                if (!TcpClient.Connected)
+                {
+                    TcpClient?.Close();
+                    TcpClient = new TcpClient();
+
+                    try
+                    {
+                        TcpClient.Connect(Ip, Port);
+                        Reader = new StreamReader(TcpClient.GetStream());
+                        Writer = new StreamWriter(TcpClient.GetStream());
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        TcpClient = new TcpClient();
+                    }
+                    catch (Exception)
+                    {
+                        // server is maybe not running or whatever
+                    }
+                }
+
+                if (TcpClient?.Client != null)
+                {
+                    IsConnected = TcpClient.Connected;
+                }
             }
         }
     }
-}
