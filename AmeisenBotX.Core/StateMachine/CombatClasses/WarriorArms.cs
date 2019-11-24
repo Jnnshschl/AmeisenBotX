5e2964e436f2ef7e06b1cbd5ef4f7d7fca318156
finalcommit <waddat@web.de>
2019-11-24 13:45:26 +0100

    one close combat fix was missing

--- AmeisenBotX.Core/StateMachine/CombatClasses/WarriorArms.cs
+++ AmeisenBotX.Core/StateMachine/CombatClasses/WarriorArms.cs
@@ -216,7 +216,7 @@ namespace AmeisenBotX.Core.StateMachine.CombatClasses
                                 }
                             }
                         }
-                        else
+                        else if (distanceToTarget < 6)
                         {
                             // -- close combat --
                             // Battle Stance
