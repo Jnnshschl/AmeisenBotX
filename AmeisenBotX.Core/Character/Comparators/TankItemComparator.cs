2aceffc7f5759422a0711d7c2599a1b2b9635b13
finalcommit <waddat@web.de>
2019-11-24 17:37:46 +0100

    rating in double and some edge case handling

--- AmeisenBotX.Core/Character/Comparators/TankItemComparator.cs
+++ AmeisenBotX.Core/Character/Comparators/TankItemComparator.cs
@@ -7,40 +7,52 @@ namespace AmeisenBotX.Core.Character.Comparators
     {
         public bool IsBetter(IWowItem current, IWowItem item)
         {
-            if(current == null)
+            if (item == null)
+            {
+                return false;
+            }
+            else if (current == null)
             {
                 return true;
             }
-            float currentRating = GetRating(current, current.EquipSlot);
-            float newItemRating = GetRating(item, current.EquipSlot);
+            else if(item.Stats == null)
+            {
+                return false;
+            }
+            else if (current.Stats == null)
+            {
+                return true;
+            }
+            double currentRating = GetRating(current, current.EquipSlot);
+            double newItemRating = GetRating(item, current.EquipSlot);
             return currentRating < newItemRating;
         }
 
-        private float GetRating(IWowItem item, EquipmentSlot slot)
+        private double GetRating(IWowItem item, EquipmentSlot slot)
         {
-            float rating = 0f;
+            double rating = 0;
             if (slot.Equals(EquipmentSlot.INVSLOT_OFFHAND))
             {
                 // shields
                 if (item.GetType() == typeof(WowArmor) && ((WowArmor) item).ArmorType.Equals(ArmorType.SHIEDLS))
                 {
-                    if (item.Stats.TryGetValue("RESISTANCE0_NAME", out string armorString) && float.TryParse(armorString, out float armor))
+                    if (item.Stats.TryGetValue("RESISTANCE0_NAME", out string armorString) && double.TryParse(armorString, out double armor))
                     {
-                        rating += 0.5f * armor;
+                        rating += 0.5 * armor;
                     }
-                    if (item.Stats.TryGetValue("ITEM_MOD_STAMINA_SHORT", out string staminaString) && float.TryParse(staminaString, out float stamina))
+                    if (item.Stats.TryGetValue("ITEM_MOD_STAMINA_SHORT", out string staminaString) && double.TryParse(staminaString, out double stamina))
                     {
                         rating += 1f * stamina;
                     }
-                    if (item.Stats.TryGetValue("ITEM_MOD_DEFENSE_SKILL_RATING_SHORT", out string defenseString) && float.TryParse(defenseString, out float defense))
+                    if (item.Stats.TryGetValue("ITEM_MOD_DEFENSE_SKILL_RATING_SHORT", out string defenseString) && double.TryParse(defenseString, out double defense))
                     {
                         rating += 1f * defense;
                     }
-                    if (item.Stats.TryGetValue("ITEM_MOD_BLOCK_VALUE_SHORT", out string blockString) && float.TryParse(blockString, out float blockValue))
+                    if (item.Stats.TryGetValue("ITEM_MOD_BLOCK_VALUE_SHORT", out string blockString) && double.TryParse(blockString, out double blockValue))
                     {
                         rating += 5f * blockValue;
                     }
-                    if (item.Stats.TryGetValue("ITEM_MOD_BLOCK_RATING_SHORT", out string blockChanceString) && float.TryParse(blockChanceString, out float blockChance))
+                    if (item.Stats.TryGetValue("ITEM_MOD_BLOCK_RATING_SHORT", out string blockChanceString) && double.TryParse(blockChanceString, out double blockChance))
                     {
                         rating += 10f * blockChance;
                     }
@@ -51,19 +63,19 @@ namespace AmeisenBotX.Core.Character.Comparators
                 // swords
                 if (item.GetType() == typeof(WowWeapon) && ((WowWeapon)item).WeaponType.Equals(WeaponType.ONEHANDED_SWORDS))
                 {
-                    if (item.Stats.TryGetValue("ITEM_MOD_STRENGTH_SHORT", out string strengthString) && float.TryParse(strengthString, out float strength))
+                    if (item.Stats.TryGetValue("ITEM_MOD_STRENGTH_SHORT", out string strengthString) && double.TryParse(strengthString, out double strength))
                     {
                         rating += 0.5f * strength;
                     }
-                    if (item.Stats.TryGetValue("ITEM_MOD_DAMAGE_PER_SECOND_SHORT", out string dpsString) && float.TryParse(dpsString, out float dps))
+                    if (item.Stats.TryGetValue("ITEM_MOD_DAMAGE_PER_SECOND_SHORT", out string dpsString) && double.TryParse(dpsString, out double dps))
                     {
                         rating += 1f * dps;
                     }
-                    if (item.Stats.TryGetValue("ITEM_MOD_DEFENSE_SKILL_RATING_SHORT", out string defenseString) && float.TryParse(defenseString, out float defense))
+                    if (item.Stats.TryGetValue("ITEM_MOD_DEFENSE_SKILL_RATING_SHORT", out string defenseString) && double.TryParse(defenseString, out double defense))
                     {
                         rating += 5f * defense;
                     }
-                    if (item.Stats.TryGetValue("ITEM_MOD_PARRY_RATING_SHORT", out string parryString) && float.TryParse(parryString, out float parry))
+                    if (item.Stats.TryGetValue("ITEM_MOD_PARRY_RATING_SHORT", out string parryString) && double.TryParse(parryString, out double parry))
                     {
                         rating += 10f * parry;
                     }
@@ -74,11 +86,11 @@ namespace AmeisenBotX.Core.Character.Comparators
                 || slot.Equals(EquipmentSlot.INVSLOT_TRINKET2))
             {
                 // jewelry stats
-                if (item.Stats.TryGetValue("ITEM_MOD_STRENGTH_SHORT", out string strengthString) && float.TryParse(strengthString, out float strength))
+                if (item.Stats.TryGetValue("ITEM_MOD_STRENGTH_SHORT", out string strengthString) && double.TryParse(strengthString, out double strength))
                 {
                     rating += 0.5f * strength;
                 }
-                if (item.Stats.TryGetValue("ITEM_MOD_STAMINA_SHORT", out string staminaString) && float.TryParse(staminaString, out float stamina))
+                if (item.Stats.TryGetValue("ITEM_MOD_STAMINA_SHORT", out string staminaString) && double.TryParse(staminaString, out double stamina))
                 {
                     rating += 1f * stamina;
                 }
@@ -86,31 +98,31 @@ namespace AmeisenBotX.Core.Character.Comparators
             else
             {
                 // armor stats
-                if (item.Stats.TryGetValue("RESISTANCE0_NAME", out string armorString) && float.TryParse(armorString, out float armor))
+                if (item.Stats.TryGetValue("RESISTANCE0_NAME", out string armorString) && double.TryParse(armorString, out double armor))
                 {
                     rating += 0.5f * armor;
                 }
-                if (item.Stats.TryGetValue("ITEM_MOD_STRENGTH_SHORT", out string strengthString) && float.TryParse(strengthString, out float strength))
+                if (item.Stats.TryGetValue("ITEM_MOD_STRENGTH_SHORT", out string strengthString) && double.TryParse(strengthString, out double strength))
                 {
                     rating += 0.5f * strength;
                 }
-                if (item.Stats.TryGetValue("ITEM_MOD_PARRY_RATING_SHORT", out string parryString) && float.TryParse(parryString, out float parry))
+                if (item.Stats.TryGetValue("ITEM_MOD_PARRY_RATING_SHORT", out string parryString) && double.TryParse(parryString, out double parry))
                 {
                     rating += 0.5f * parry;
                 }
-                if (item.Stats.TryGetValue("ITEM_MOD_BLOCK_RATING_SHORT", out string blockChanceString) && float.TryParse(blockChanceString, out float blockChance))
+                if (item.Stats.TryGetValue("ITEM_MOD_BLOCK_RATING_SHORT", out string blockChanceString) && double.TryParse(blockChanceString, out double blockChance))
                 {
                     rating += 0.5f * blockChance;
                 }
-                if (item.Stats.TryGetValue("ITEM_MOD_BLOCK_VALUE_SHORT", out string blockString) && float.TryParse(blockString, out float blockValue))
+                if (item.Stats.TryGetValue("ITEM_MOD_BLOCK_VALUE_SHORT", out string blockString) && double.TryParse(blockString, out double blockValue))
                 {
                     rating += 0.5f * blockValue;
                 }
-                if (item.Stats.TryGetValue("ITEM_MOD_STAMINA_SHORT", out string staminaString) && float.TryParse(staminaString, out float stamina))
+                if (item.Stats.TryGetValue("ITEM_MOD_STAMINA_SHORT", out string staminaString) && double.TryParse(staminaString, out double stamina))
                 {
                     rating += 1f * stamina;
                 }
-                if (item.Stats.TryGetValue("ITEM_MOD_DEFENSE_SKILL_RATING_SHORT", out string defenseString) && float.TryParse(defenseString, out float defense))
+                if (item.Stats.TryGetValue("ITEM_MOD_DEFENSE_SKILL_RATING_SHORT", out string defenseString) && double.TryParse(defenseString, out double defense))
                 {
                     rating += 1f * defense;
                 }
