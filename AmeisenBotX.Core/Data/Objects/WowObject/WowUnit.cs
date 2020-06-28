555e0ff559946a6b4088e4d21d7a550b4f66dc05
Jannis Höschele <jannis@jnns.de>
2020-06-28 21:15:43 +0200

    tweak movement

--- AmeisenBotX.Core/Data/Objects/WowObject/WowUnit.cs
+++ AmeisenBotX.Core/Data/Objects/WowObject/WowUnit.cs
@@ -189,10 +189,6 @@ namespace AmeisenBotX.Core.Data.Objects.WowObject
         {
             return Auras != null && Auras.Any(e => e.Name == name);
         }
-        public bool HasBuffById(int spellId)
-        {
-            return Auras != null && Auras.Any(e => e.SpellId == spellId);
-        }
 
         public bool IsInMeleeRange(WowUnit wowUnit) => Position.GetDistance(wowUnit.Position) < Math.Max(4.5, CombatReach + wowUnit.CombatReach + 1);
 
