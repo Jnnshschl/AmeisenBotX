9f803df21f82cce8d066da94c057af3eebe70dcd
Jannis Höschele <jannis@jnns.de>
2020-08-21 18:04:07 +0200

    small safety tweaks

--- AmeisenBotX/MainWindow.xaml.cs
+++ AmeisenBotX/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
