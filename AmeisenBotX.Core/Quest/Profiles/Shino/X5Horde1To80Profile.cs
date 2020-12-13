8e8ef01bb47e24edf6bd40a5dcd751693b3ba04e
Geigerkind <tom@dymel.dev>
2020-12-13 23:07:41 +0100

    Valley Of Strength cutting teeth

---
+++ AmeisenBotX.Core/Quest/Profiles/Shino/X5Horde1To80Profile.cs
@@ -0,0 +1,21 @@
+﻿using AmeisenBotX.Core.Quest.Objects.Quests;
+using AmeisenBotX.Core.Quest.Quests.StartArea;
+using System.Collections.Generic;
+
+namespace AmeisenBotX.Core.Quest.Profiles.Shino
+{
+    class X5Horde1To80Profile : IQuestProfile
+    {
+        public Queue<List<BotQuest>> Quests { get; }
+
+        public X5Horde1To80Profile(WowInterface wowInterface)
+        {
+            var valleyOfStrength = new ValleyOfStrength(wowInterface);
+
+            Quests = new Queue<List<BotQuest>>();
+            Quests.Enqueue(new List<BotQuest>() {
+                valleyOfStrength.QCuttingTeeth
+            });
+        }
+    }
+}
---
+++ AmeisenBotX.Core/Quest/Quests/StartArea/ValleyOfStrength.cs
@@ -0,0 +1,32 @@
+﻿using AmeisenBotX.Core.Movement.Pathfinding.Objects;
+using AmeisenBotX.Core.Quest.Objects.Objectives;
+using AmeisenBotX.Core.Quest.Objects.Quests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmeisenBotX.Core.Quest.Quests.StartArea
+{
+    class ValleyOfStrength
+    {
+        public BotQuest QCuttingTeeth;
+        
+        public ValleyOfStrength(WowInterface wowInterface)
+        {
+            QCuttingTeeth = new BotQuest(
+                wowInterface, 788, "Cutting Teeth", 55, 1,
+                () => (wowInterface.ObjectManager.GetClosestWowUnitByDisplayId(new List<int> { 1653 }), new Vector3(-600, -4186, 41)),
+                () => (wowInterface.ObjectManager.GetClosestWowUnitByDisplayId(new List<int> { 1653 }), new Vector3(-600, -4186, 41)),
+                new List<IQuestObjective>()
+                {
+                    new QuestObjectiveChain(new List<IQuestObjective>()
+                    {
+                        new MoveToPositionQuestObjective(wowInterface, new Vector3(-375, -4314, 51), 80.0),
+                        new KillUnitQuestObjective(wowInterface, new Dictionary<int, int> { { 0, 503 } }, () => wowInterface.ObjectManager.Player.QuestlogEntries.FirstOrDefault(e => e.Id == 788).Finished == 1)
+                    })
+                });
+        }
+    }
+}
