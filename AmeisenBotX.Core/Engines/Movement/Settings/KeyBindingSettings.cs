29c29fa72ab560fb398787c793316d51db84cf53
iEEcor <imod211-rf@yahoo.com>
2021-08-05 06:00:23 +0200

    Added start stop keybinding to the config (#38)
    * Fixed event subscription to let the event bubble and code cleanup
    
    * Added start stop keybinding to the config
    
    Co-authored-by: iMod <imod@xcor.org>

---
+++ AmeisenBotX.Core/Engines/Movement/Settings/KeyBindingSettings.cs
@@ -0,0 +1,22 @@
+﻿using AmeisenBotX.Core.Keyboard;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmeisenBotX.Core.Engines.Movement.Settings
+{
+    public class KeyBindingSettings
+    {
+        #region Public
+
+        /// <summary>
+        /// Key bindings for starting or stopping the bot.
+        /// </summary>
+        public (VirtualKeyStates, Keys) StartStopBot { get; set; } = (VirtualKeyStates.LALT, Keys.X);
+
+        #endregion
+    }
+}
