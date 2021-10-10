edafb0f6b1a9eead5cbffa4482587fd86e10acee
Jannis Höschele <jannis@jnns.de>
2021-10-10 12:18:11 +0200

    fix dungeon partymode bug

--- AmeisenBotX.Core/Logic/AmeisenBotLogic.cs
+++ AmeisenBotX.Core/Logic/AmeisenBotLogic.cs
@@ -990,17 +990,16 @@ namespace AmeisenBotX.Core.Logic
 
         private bool NeedToRepairOrSell()
         {
-            bool needToRepair = Bot.Character.Equipment.Items.Any(e =>
-                e.Value.MaxDurability > 0 && e.Value.Durability / (double)e.Value.MaxDurability * 100.0 <= Config.ItemRepairThreshold);
+            bool needToRepair = Bot.Character.Equipment.Items.Any(e => e.Value.MaxDurability > 0 && e.Value.Durability / (double)e.Value.MaxDurability * 100.0 <= Config.ItemRepairThreshold);
 
             bool needToSell = Bot.Character.Inventory.FreeBagSlots < Config.BagSlotsToGoSell
                               && Bot.Character.Inventory.Items
                               .Any(e => e.Price > 0 && !Config.ItemSellBlacklist.Contains(e.Name)
-                                      && (Config.SellGrayItems && e.ItemQuality == (int)WowItemQuality.Poor
-                                      || Config.SellWhiteItems && e.ItemQuality == (int)WowItemQuality.Common
-                                      || Config.SellGreenItems && e.ItemQuality == (int)WowItemQuality.Uncommon
-                                      || Config.SellBlueItems && e.ItemQuality == (int)WowItemQuality.Rare
-                                      || Config.SellPurpleItems && e.ItemQuality == (int)WowItemQuality.Epic));
+                                      && ((Config.SellGrayItems && e.ItemQuality == (int)WowItemQuality.Poor)
+                                      || (Config.SellWhiteItems && e.ItemQuality == (int)WowItemQuality.Common)
+                                      || (Config.SellGreenItems && e.ItemQuality == (int)WowItemQuality.Uncommon)
+                                      || (Config.SellBlueItems && e.ItemQuality == (int)WowItemQuality.Rare)
+                                      || (Config.SellPurpleItems && e.ItemQuality == (int)WowItemQuality.Epic)));
 
             IWowUnit vendorRepair = null;
             IWowUnit vendorSell = null;
